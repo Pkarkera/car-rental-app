105a352dec8d9350f6fb6129e8858b93ba9e07aa
prithvikarkera <pkarkera99@gmail.com>
2023-10-25 20:58:18 -0400

    Initial commit1

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,25 +7,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration (Naitik)
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyB3oTV1dp5h-8VSiJpOx0JOv2fvgzQc8Qc',
-//   authDomain: 'group07-project-rn.firebaseapp.com',
-//   projectId: 'group07-project-rn',
-//   storageBucket: 'group07-project-rn.appspot.com',
-//   messagingSenderId: '565444894885',
-//   appId: '1:565444894885:web:ee4335d4b665b5edd12d4c',
-// };
-
-//Dilpreet firebase config
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: 'AIzaSyBZkM7ZwPgFaly36o7pWnbTdWPTQ-ut-v4',
-  // authDomain: 'ownerappg07.firebaseapp.com',
-  // projectId: 'ownerappg07',
-  // storageBucket: 'ownerappg07.appspot.com',
-  // messagingSenderId: '222437232722',
-  // appId: '1:222437232722:web:40eeaa6b4c36983be4cf8b',
   apiKey: "AIzaSyC-HLsnhAZkeJJ-avUt3TzcO34yCIL-wgw",
   authDomain: "rentalapp-e00cc.firebaseapp.com",
   projectId: "rentalapp-e00cc",
